75feb621eea853fbad4e4c94d391530e6a374360
BorysSerbyn <borys.serbyn@gmail.com>
2020-12-17 20:05:30 -0500

    over the board completed

--- src/borys/serbyn/Tile.java
+++ src/borys/serbyn/Tile.java
@@ -2,43 +2,22 @@ package borys.serbyn;
 
 import java.awt.*;
 
-public class Tile implements Cloneable{
-    private int x;
-    private int y;
-    private Color color;
+public record Tile(int x, int y, Color color) implements Cloneable {
 
-    public Tile(int x, int y, Color color) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
-
-    @Override
-    public Object clone() {
+@Override
+public Object clone() {
         try {
-            return (Tile) super.clone();
+        return (Tile) super.clone();
         } catch (CloneNotSupportedException e) {
-            return new Tile(this.x, this.y, this.getColor());
+        return new Tile(this.x, this.y, this.getColor());
         }
-    }
+        }
 
-    @Override
-    public String toString() {
+@Override
+public String toString() {
         return "Tile{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public Color getColor() {
-        return color;
-    }
+        "x=" + x +
+        ", y=" + y +
+        '}';
+        }
 }
